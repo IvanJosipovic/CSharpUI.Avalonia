f183922f23351ea7b0836b68ffb95c012a1e2dcd
Ivan Josipovic <9521987+IvanJosipovic@users.noreply.github.com>
2025-09-05 23:28:49 -0700

    wip

--- src/CSharpUI.Avalonia.SourceGenerator/Generators/SetterGenerators/ValueSetterGenerator.cs
+++ src/CSharpUI.Avalonia.SourceGenerator/Generators/SetterGenerators/ValueSetterGenerator.cs
@@ -4,7 +4,7 @@ namespace CSharpUI.Avalonia.SourceGenerator.Generators.SetterGenerators;
 
 public class ValueSetterGenerator : ExtensionGeneratorBase<PropertyExtensionInfo>
 {
-    protected override string? GetExtension(PropertyExtensionInfo info) => $@"    /// <summary>{info.Comment}</summary>
-    public static {info.ReturnType} {info.ExtensionName}{info.GenericArg}(this {info.ReturnType} control, {info.ValueTypeSource} value){info.GenericConstraint}
-        => control._set(() => control.{info.MemberName} = value!);";
+    protected override string? GetExtension(PropertyExtensionInfo info) => $@"    /// <summary>{info.Comment}</summary>{Extensions.NewLine}"
+    + $"    public static {info.ReturnType} {info.ExtensionName}{info.GenericArg}(this {info.ReturnType} control, {info.ValueTypeSource} value){info.GenericConstraint}{Extensions.NewLine}"
+    + $"        => control._set(() => control.{info.MemberName} = value!);";
 }
