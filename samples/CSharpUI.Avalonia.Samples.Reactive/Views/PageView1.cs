e605c4d68c5f406f5925b89833709c821ab1b968
Ivan Josipovic <9521987+IvanJosipovic@users.noreply.github.com>
2025-09-07 19:58:08 -0700

    chore: update sample

--- samples/CSharpUI.Avalonia.Samples.Reactive/Views/PageView1.cs
+++ samples/CSharpUI.Avalonia.Samples.Reactive/Views/PageView1.cs
@@ -1,6 +1,4 @@
-﻿using Avalonia.Styling;
 using CSharpUI.Avalonia.Samples.Reactive.ViewModels;
-using CSharpUI.Avalonia.Styles;
 
 namespace CSharpUI.Avalonia.Samples.Reactive.Views;
 
@@ -8,21 +6,22 @@ public class PageView1 : ReactiveViewBase<PageViewModel1>
 {
     protected override StyleGroup? BuildStyles() =>
     [
-         new Style(x => x.Is<TextBlock>())
-             .Setter(HorizontalAlignmentProperty, HorizontalAlignment.Center)
-             .Setter(VerticalAlignmentProperty, VerticalAlignment.Center),
-         new Style(x => x.Is<TextBox>().Class("myTextBox"))
-             .Setter(HorizontalAlignmentProperty, HorizontalAlignment.Center)
-             .Setter(VerticalAlignmentProperty, VerticalAlignment.Center),
+         new Style<TextBlock>()
+            .HorizontalAlignment(HorizontalAlignment.Center)
+            .VerticalAlignment(VerticalAlignment.Center),
+
+         new Style<TextBox>(x => x.Class("myTextBox"))
+            .HorizontalAlignment(HorizontalAlignment.Right)
+            .VerticalAlignment(VerticalAlignment.Center)
     ];
 
     protected override Control Build(PageViewModel1 vm) =>
         new StackPanel()
             .Children([
                 new TextBlock()
-                    .ReactiveBinding(TextBox.TextProperty, vm, x => x.MyProperty),
+                    .ReactiveBinding(TextBox.TextProperty, vm, x => x.MyProperty), // One way
                 new TextBox()
                     .Class("myTextBox")
-                    .ReactiveBinding(TextBox.TextProperty, vm, x => x.MyProperty, x => vm.MyProperty = x ?? ""),
+                    .ReactiveBinding(TextBox.TextProperty, vm, x => x.MyProperty, x => vm.MyProperty = x ?? ""), // Two way
             ]);
 }
